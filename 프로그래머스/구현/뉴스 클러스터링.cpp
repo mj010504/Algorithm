2889a26d8fa740842993537d0ad4672b192e51d8
최민준 <68235719+mj010504@users.noreply.github.com>
2026-01-01 23:24:43 +0900

    회전 로직 수정

--- 프로그래머스/구현/뉴스 클러스터링.cpp
+++ 프로그래머스/구현/뉴스 클러스터링.cpp
@@ -2,56 +2,51 @@
 
 using namespace std;
 
-bool isSpecial(char c) {
-    if(c >= 'a' && c <= 'z') return false;
-    if(c >= 'A' && c <= 'Z') return false;
-    return true;
+int rotate(vector<vector<int>> &v, vector<int> q) {
+    int minn = INT_MAX;
+    int x = q[0] - 1, y = q[1] - 1, xx = q[2] - 1, yy = q[3] - 1;
+    auto tv = v;
+
+    for(int i = y + 1; i <= yy; i++) {
+        tv[x][i] = v[x][i - 1];
+        minn = min(v[x][i - 1], minn);
+    }
+
+    for(int i = x + 1; i <= xx; i++) {
+        tv[i][yy] = v[i - 1][yy];
+        minn = min(v[i- 1][yy], minn);
+    }
+
+    for(int i = yy - 1; i >= y; i--) {
+        tv[xx][i] = v[xx][i + 1];
+        minn = min(minn, v[xx][i + 1]);
+    }
+
+    for(int i = xx - 1; i >= x; i--) {
+        tv[i][y] = v[i + 1][y];
+        minn = min(minn, v[i + 1][y]);
+    }
+
+    v = tv;
+    return minn;
 }
 
-int solution(string str1, string str2) {
-    int res = 0;
-    map<string, int> a, b;
-    for(int i = 0; i < str1.size() - 1; i++) {
-        string temp = "";
-        temp += tolower(str1[i]); temp += tolower(str1[i + 1]);
-        if(!isSpecial(temp[0]) && !isSpecial(temp[1])) {
-            a[temp] += 1;
+vector<int> solution(int r, int h, vector<vector<int>> qv) {
+    vector<int> res;
+    vector<vector<int>> v(r, vector<int> (h));
+
+    int temp = 1;
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < h; j++) {
+            v[i][j] = temp;
+            temp++;
         }
     }
 
-    for(int i = 0; i < str2.size() - 1; i++) {
-        string temp = "";
-        temp += tolower(str2[i]); temp += tolower(str2[i + 1]);
-        if(!isSpecial(temp[0]) && !isSpecial(temp[1])) {
-            b[temp] += 1;
-        }
-    }   
-
-    if(a.size() == 0 && b.size() == 0) return 65536;
-
-    // 교집합, 합집합
-    int g = 0, h = 0;
-    for(auto aa : a) {
-        int temp = min(aa.second, b[aa.first]);
-        g += temp;
+    for(auto q : qv) {
+        int minn = rotate(v, q); 
+        res.push_back(minn);
     }
-
-    // 합집합 사용된 원소 구분
-    map<string, int> m;
-    for(auto aa : a) {
-        m[aa.first] = 1;
-        int temp = max(aa.second, b[aa.first]);
-        h += temp;
-    }
-
-    for(auto bb : b) {
-        if(m[bb.first] != 1) {
-            h += bb.second;
-        }
-    }
-
-    double temp = (double)g / (double)h;
-    res = temp * 65536;
 
     return res;
 }
